a9ef52db057b1acb093db95c05ce3a529a0cd2ce
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-11-21 17:21:51 +0200

    create config module

--- src/apollo/apollo.module.ts
+++ src/apollo/apollo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppConfigModule } from 'src/app-config/app-config.module';
 import { ApolloService } from './apollo.service';
 
 @Module({
   providers: [ApolloService],
   exports: [ApolloService],
+  imports: [AppConfigModule],
 })
 export class ApolloModule {}
