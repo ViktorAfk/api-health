9ac17fbc331b1ef04d74bdc6f1a86b8a81caac6c
Viktor Shaparenko <vkshadyafk@gmail.com>
2024-11-21 12:42:13 +0200

    setup apollo client

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ApolloModule } from 'src/apollo/apollo.module';
+import { GraphQLRequestModule } from 'src/graphql-request/graphql-request.module';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
 
 @Module({
   controllers: [HealthController],
-  providers: [HealthService]
+  providers: [HealthService],
+  imports: [GraphQLRequestModule, ApolloModule],
 })
 export class HealthModule {}
--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,8 +1,24 @@
+import { gql } from '@apollo/client/core';
 import { Injectable } from '@nestjs/common';
+import { ApolloService } from 'src/apollo/apollo.service';
 
 @Injectable()
 export class HealthService {
-  getHeathValue() {
-    return 'I am healthy';
+  constructor(private readonly apolloService: ApolloService) {}
+  getApiHeath() {
+    const query = gql`
+    query {
+    apiHealth {
+    status
+    services {
+      name
+      category
+      status
+        }
+      }
+    }
+  `;
+
+    return this.apolloService.getQuery(query);
   }
 }
